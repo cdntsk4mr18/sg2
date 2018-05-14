c53efdd2190f0395113cbf8b8352a8ae471c9a4b
jrval <valencia_jr08@live.com>
2018-05-14 23:42:10 +0800

    add sms feature

--- admin/functions/addEvent.php
+++ admin/functions/addEvent.php
@@ -29,7 +29,62 @@ if (isset($_POST['title']) && isset($_POST['start']) && isset($_POST['end']) && 
 	 die ('Erreur execute');
 	}
 
+
+//##########################################################################
+// ITEXMO SEND SMS API - PHP - CURL-LESS METHOD
+// Visit www.itexmo.com/developers.php for more info about this API
+//##########################################################################
+
+//##########################################################################
+
+$sql_2 = "SELECT mobile_number FROM users";
+//$result_2 = $db->prepare( $sql_2 );
+
+$req = $db->prepare($sql_2);
+$req->execute();
+
+$user_number = $req->fetchAll();
+
+
+
+foreach($user_number as $user_numbers): 
+			
+				
+					$number=$user_numbers['mobile_number'];
+			        $api="TR-JRVAL728059_QKJYA";
+			        $text = "What:".$title."\n".
+			        		"When:".$start."-".$end."\n".
+			        		"Time:".$time;
+
+			        $result_text = itexmo($number,$text,$api);
+
+		        	if ($result_text == ""){
+						print_r ("iTexMo: No response from server!!!
+						Please check the METHOD used (CURL or CURL-LESS). If you are using CURL then try CURL-LESS and vice versa.	
+						Please CONTACT US for help. ");	
+						}else if ($result_text == 0){
+						print_r ("Message Sent!");
+						}
+						else{	
+						print_r ("Error Num ". $result_text . " was encountered!");
+						}
+
+			endforeach;
 }
+
+function itexmo($number,$message,$apicode){
+$url = 'https://www.itexmo.com/php_api/api.php';
+$itexmo = array('1' => $number, '2' => $message, '3' => $apicode);
+$param = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($itexmo),
+    ),
+);
+$context  = stream_context_create($param);
+return file_get_contents($url, false, $context);}
+
 header('Location: '.$_SERVER['HTTP_REFERER']);
 
 	
